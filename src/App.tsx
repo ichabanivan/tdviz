4faf74a652df0fce08e047702f6e2361782196f0
zanusilker <githubJake8>
2022-04-03 22:10:18 +0300

    architecture/SH-8-Connect-antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Typography, Button } from 'antd';
+
 import './App.css';
+import 'antd/dist/antd.css';
+
+const { Title } = Typography;
 
 function App() {
   return (
@@ -8,8 +13,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Title>Safe Health</Title>
         </p>
+        <Button type="primary">Primary Button</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
