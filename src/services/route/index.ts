45de6576dd2bb9ecc093e4892ed7b3e29b7b031e
Ivan <zanusilker@gmail.com>
2022-06-28 11:27:31 +0300

    workflow/SH-49-Browserlist (#51)
    Co-authored-by: zanusilker <githubJake8>

--- src/services/route/index.ts
+++ src/services/route/index.ts
@@ -1,8 +1,9 @@
+import { Route, RouteOptions } from './route';
+import { Parameter, ParameterOptions } from './parameter';
+
 export { history } from './history';
 export { NEW_ID } from './constants';
 export { ANNOTATION } from './annotations';
-import { Route, RouteOptions } from './route';
-import { Parameter, ParameterOptions } from './parameter';
 
 // eslint-disable-next-line max-len
 export const defineRoute = (url: string, relative: string, options: RouteOptions) => Route.create(url, relative, options);
